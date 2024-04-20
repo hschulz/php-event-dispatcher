dc15ecf1c4e7588925a5b23c3f69d42354675fd7
Hauke Schulz <hauke27@gmail.com>
2024-04-20 08:46:23 +0200

    Updated result handling during event dispatching
    The dispatcher now handles empty results and wrongly typed results.

--- src/EventDispatcher.php
+++ src/EventDispatcher.php
@@ -6,6 +6,7 @@ namespace Hschulz\Dispatcher;
 
 use Hschulz\Dispatcher\AbstractEventDispatcher;
 use Hschulz\Dispatcher\EventInterface;
+use RuntimeException;
 
 /**
  * Event dispatcher class.
@@ -15,11 +16,12 @@ class EventDispatcher extends AbstractEventDispatcher
     /**
      * Dispatches the provided event.
      *
+     * @throws RuntimeException If the event listener returns something else than an event
      * @param EventInterface $event The event to dispatch
      * @return EventInterface The dispatched event
      */
-    public function dispatch(EventInterface $event): EventInterface {
-
+    public function dispatch(EventInterface $event): EventInterface
+    {
         /*
          * Clone the event to prevent modification
          * and return the modified event later.
@@ -29,9 +31,21 @@ class EventDispatcher extends AbstractEventDispatcher
         $listeners = $this->listenerProvider->getListenersForEvent($event);
 
         foreach ($listeners as $listener) {
-            $temp = call_user_func($listener, $temp);
+            $result = call_user_func($listener, $temp);
 
-            if ($temp->isPropagationStopped()) {
+            /* In case the event listener doesn't return the event */
+            if ($result === null) {
+                continue;
+            }
+
+            /* In case the event listener returns something else */
+            if (!($result instanceof EventInterface)) {
+                throw new RuntimeException('Event listener must return an instance of ' . EventInterface::class);
+            }
+
+            /* The event propagation was stopped */
+            if ($result->isPropagationStopped()) {
+                $temp = $result;
                 break;
             }
         }
